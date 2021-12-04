f4f0944e8fc2bbe8b3f50b979e4ad788e40ce398
Toni Kukurin <tkukurin@gmail.com>
2021-12-04 18:58:14 +0100

    extract camelcasing to text utils

---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+import pytest
+from src.tkukurin.utils import text
+
+@pytest.mark.parametrize('input_str', [
+  'Ok Computer CAMEL-CASE',
+  'ok_computer---camel//case',
+  'ok    computer   camel \t\n case',
+])
+def test_inputStr_cleanCamelcase_cleansCamelcase(input_str):
+  assert text.clean_camelcase(input_str) == 'OkComputerCamelCase'
