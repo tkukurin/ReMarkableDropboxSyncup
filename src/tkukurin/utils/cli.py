8b5cad59ff3b660cc93471253fb65b20abb96db5
Toni Kukurin <tkukurin@gmail.com>
2021-12-04 15:23:30 +0100

    Extracted cli to utils and enabled auto doc generation (#3)

---
+++ src/tkukurin/utils/cli.py
@@ -0,0 +1,43 @@
+
+import argparse
+import typing as ty
+import inspect as I
+import logging
+
+
+def cli_from_instancemethods(
+    cls: ty.Type,
+    common_args: argparse.ArgumentParser,
+    log: logging.Logger) -> ty.Tuple[ty.Callable, dict]:
+  '''Automatically infer CLI from a method's public interface.
+
+  Returns a method to be called and corresponding args (incl `common_args`).
+  '''
+  parser = argparse.ArgumentParser()
+  subparser = parser.add_subparsers(title='cmd', required=True, dest='cmd')
+  isclassmethod = lambda x: I.ismethod(x) and x.__self__ != cls
+  methods = {
+    name: obj for name, obj in I.getmembers(cls)
+    if I.isfunction(obj) and not isclassmethod(obj) and not name.startswith('_')
+  }
+  for mname, method in methods.items():
+    mparser = subparser.add_parser(
+        mname, parents=[common_args], help=I.getdoc(method))
+    sig = I.signature(method)
+    for name, param in sig.parameters.items():
+      if name == 'self': continue
+      type_ = param.annotation if param.annotation != I._empty else str
+      default = param.default if param.default != I._empty else None
+      mparser.add_argument(f'--{name}', type=type_, default=default)
+  args = parser.parse_args().__dict__
+  cmd = args.pop('cmd')
+  method = methods[cmd]
+
+  def logged_method(*a, **kw):
+    log.debug('Starting %s: %s', cmd, kw)
+    result = method(*a, **kw)
+    log.debug('Done')
+    return result
+
+  return (logged_method if log else method), args
+
